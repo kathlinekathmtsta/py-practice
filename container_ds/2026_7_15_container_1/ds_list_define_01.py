9605ecae00ffb9dcee26979ff7048a4a137b40ca
Kathline <kathlinekathmtsta2179@gmail.com>
2026-07-30 14:57:44 +0800

    整理并重命名ds_list相关文件

--- container_ds/2026_7_15_container_1/ds_list_define_01.py
+++ container_ds/2026_7_15_container_1/ds_list_define_01.py
@@ -29,3 +29,23 @@ print(new_list, stu_names)
 #   列表的倒序
 new_list = stu_names[::-1]
 print(new_list, stu_names)
+
+"""
+python中有内置的几种数据结构：列表、字典、元组、集合
+数据结构可以保存多个数据
+
+list定义的方式：
+    data_list = []  代表创建了一个空列表
+    data_list = list()  代表创建了一个空列表"""
+
+# 列表可以保存多个不同类型的数据
+data_list = [1, 2, 3, 4, '123', 3.14, True, False]
+print(data_list)
+
+# 空列表的声明方式
+data_list = []
+print(data_list)
+
+#   常用
+data_list = list()
+print(data_list)
